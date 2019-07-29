fe03667e0ce4b14eeec58f7d81e2baf1ab1f38ad
Sanidhya Samadhiya <sanidhyasamadhiya3@gmail.com>
2019-07-29 19:30:07 +0530

    FirstDeploy

---
+++ server.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const cors=require('cors');
+const bodyParser=require('body-parser');
+const PORT=process.env.port || 3000;
+const app=express()
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/',(req,res)=>{
+    res.send("This is working Fine I am Happy")
+})
+
+
+app.listen(PORT,()=>{
+    console.log("App is running ")
+})
