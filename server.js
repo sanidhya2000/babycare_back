b013efba09f5ffab8c31426e0526eb3ff868d8f6
Sanidhya Samadhiya <sanidhyasamadhiya3@gmail.com>
2019-07-30 07:27:02 +0530

    add

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const cors=require('cors');
 const bodyParser=require('body-parser');
-const PORT=process.env.port || 3000;
+const PORT=process.env.PORT || 3000;
 const app=express()
 app.use(bodyParser.json());
 app.use(cors());
